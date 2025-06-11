3cfe688a52bf7dd9a4656bb4619b15874912d7f3
shiva4sw <shiva.4sw@gmail.com>
2025-06-11 16:22:42 +0530

    'com2'

--- blog/forms.py
+++ blog/forms.py
@@ -7,3 +7,11 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'content']
+
+        widgets = {
+            'title':forms.TextInput(attrs={
+                'placeholder':'Title',
+                'class':'input-box',
+                'required':''
+                })
+        }
