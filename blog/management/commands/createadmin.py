1a19a49d3da61e7330b7c43adce147e453010115
shiva4sw <shiva.4sw@gmail.com>
2025-06-11 19:46:32 +0530

    'com2'

---
+++ blog/management/commands/createadmin.py
@@ -0,0 +1,16 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+import os
+
+class Command(BaseCommand):
+    def handle(self, *args, **kwargs):
+        User = get_user_model()
+        username = os.getenv("DJANGO_SUPERUSER_USERNAME", "admin")
+        email = os.getenv("DJANGO_SUPERUSER_EMAIL", "admin@example.com")
+        password = os.getenv("DJANGO_SUPERUSER_PASSWORD", "admin123")
+
+        if not User.objects.filter(username=username).exists():
+            User.objects.create_superuser(username=username, email=email, password=password)
+            self.stdout.write(self.style.SUCCESS("Superuser created"))
+        else:
+            self.stdout.write(self.style.WARNING("Superuser already exists"))
