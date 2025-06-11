a8efec15f9f7dcd1262afa238e62d61c86d4ad86
shiva4sw <shiva.4sw@gmail.com>
2025-06-11 14:26:31 +0530

    'com2'

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-y!v3p2ewj*=k!nbizb=5)3(o+7mwf#@t*x9-_km#6i@xhr@i25'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
